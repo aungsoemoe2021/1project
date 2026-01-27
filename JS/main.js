bd9e9d23b18720610d1ffad5dd962efb236ac0b8
aungsoemoe2021 <tigermanreal7@gmail.com>
2026-01-27 15:22:50 +0630

    first commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show_menu");
+});
+
